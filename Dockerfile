46de229aaec983dc18198746f27b347c26594c08
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2019-01-17 16:28:18 +0100

    reduced image size + refactoring

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,7 @@ WORKDIR /opt
 COPY build_qemu.sh /opt/build_qemu.sh
 RUN ./build_qemu.sh "qemu-3.0.0"
 ENV PATH="/opt/qemu-targets:${PATH}"
+
+RUN apt-get remove gcc cmake make gdb libtool-bin wget python automake bison git \
+    libssl-dev nettle-dev flex python3 build-essential python3-setuptools python3-dev screen libpixman-1-dev && \
+    apt-get autoremove && apt-get clean
--- build_qemu.sh
+++ build_qemu.sh
@@ -4,15 +4,15 @@ QEMU_VERSION=$1
 
 cd /opt
 mkdir /opt/qemu-targets
-wget https://download.qemu.org/${QEMU_VERSION}.tar.xz
-tar -xvf ${QEMU_VERSION}.tar.xz
+wget -nv https://download.qemu.org/${QEMU_VERSION}.tar.xz
+tar -xf ${QEMU_VERSION}.tar.xz
 rm ${QEMU_VERSION}.tar.xz
 cd ${QEMU_VERSION}
 
 # possible targets: aarch64 alpha arm armeb cris i386 m68k microblaze microblazeel mips mips64 mips64el mipsel mipsn32 mipsn32el or32 ppc ppc64 ppc64abi32 ppc64le s390x sh4 sh4eb sparc sparc32plus sparc64 unicore32 x86_64
 CPU_TARGETS="aarch64 alpha arm armeb i386 m68k mips mips64 mips64el mipsel mipsn32 mipsn32el ppc ppc64 ppc64abi32 ppc64le s390x sh4 sh4eb sparc sparc32plus sparc64 x86_64"
 
-for CPU_TARGET in $CPU_TARGETS; do
+for CPU_TARGET in ${CPU_TARGETS}; do
 
     echo "[*] Configuring QEMU for $CPU_TARGET..."
 
@@ -20,15 +20,12 @@ for CPU_TARGET in $CPU_TARGETS; do
       --disable-gtk --disable-sdl --disable-vnc \
       --target-list="${CPU_TARGET}-linux-user" || exit 1
 
-    echo "[+] Configuration complete."
-
-    echo "[*] Attempting to build QEMU (fingers crossed!)..."
-
     make || exit 1
 
     echo "[+] Build process successful!"
 
-    echo "[*] Copying binary..."
-
     cp -f "${CPU_TARGET}-linux-user/qemu-${CPU_TARGET}" "/opt/qemu-targets" || exit 1
 done
+
+cd ..
+rm -rf ${QEMU_VERSION}
